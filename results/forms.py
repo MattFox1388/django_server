e21f7b23a411fd683d71a81137975285a324cd5a
MattFox1388 <fox5@uwm.edu>
2019-03-17 22:43:00 -0500

    added forms.py

--- results/forms.py
+++ results/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+FILE_CHOICES = (
+    ('text', 'TEXT'),
+    ('audio', 'AUDIO'),
+    ('video', 'VIDEO'),
+    ('images', 'IMAGES')
+)
+
 class SearchForm(forms.Form):
     search_phrase = forms.CharField(max_length=128)
+    files = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=FILE_CHOICES)
--- results/views.py
+++ results/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views import View
+from .forms import SearchForm
 # Create your views here.
+
+
+class HomeView(View):
+    template_name = 'index.html'
+    search_form = SearchForm
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': self.search_form})
+
+    def post(self, request):
+        pass
