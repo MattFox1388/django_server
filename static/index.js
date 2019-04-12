e25ef286b0a46ee6998ea40a45b25dd6d35b0875
MattFox1388 <fox5@uwm.edu>
2019-04-12 01:11:06 -0500

    added csrf token

--- static/index.js
+++ static/index.js
@@ -1,4 +1,20 @@
 $(function(){
+    //setting up csrf token for ajax requests
+    csrftoken = Cookies.get('csrftoken');
+
+    function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        crossDomain: false, // obviates need for sameOrigin test
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
     //is called when a file link is clicked
     $('.file_link').click(function(){
         let filePath = $(this).text();
