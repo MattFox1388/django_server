a1ce7b579de76c55994f0203fa7981648882f0c4
MattFox1388 <fox5@uwm.edu>
2019-05-01 09:33:26 -0500

    added script to html (https://stackoverflow.com/questions/298772/django-template-variables-and-javascript)

--- static/inspect.js
+++ static/inspect.js
@@ -2,10 +2,6 @@ $(function(){
     //setting up csrf token for ajax requests
     csrftoken = Cookies.get('csrftoken');
 
-    //get tags and add to the tagsinput
-    var js_tags = "{{js_tags|safe}}";
-    console.log('js_tags: ' + js_tags);
-
     function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
